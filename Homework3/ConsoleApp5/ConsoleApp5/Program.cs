fe580dab01f3e0cca083953c5607560eb68a3a0e
Hallimede <1216938422@qq.com>
2020-03-07 20:35:23 +0800

    Homework03

---
+++ Homework3/ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    
+    class CreateShape
+    {
+        public static Shape Factory(int shapeNum)
+        {
+            Shape shape;
+            Random rd = new Random();
+            switch (shapeNum)
+            {
+                case 1:
+                    shape = new Rectangle(10*rd.NextDouble(), 10 * rd.NextDouble());
+                    Console.WriteLine(shape.ToString());
+                    break;
+                case 2:
+                    shape = new Square(10 * rd.NextDouble());
+                    Console.WriteLine(shape.ToString());
+                    break;
+                case 3:
+                    shape = new Triangle(10 * rd.NextDouble(), 10 * rd.NextDouble());
+                    Console.WriteLine(shape.ToString());
+                    break;
+                default:
+                    shape = null;
+                    Console.WriteLine(shape.ToString());
+                    break;
+            }
+            return shape;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rd = new Random();
+
+            Shape []shapes = new Shape[10];
+            for(int i = 0; i < 10; i++)
+            {
+                shapes[i] = CreateShape.Factory(rd.Next(1, 3));
+            }
+
+            double sumArea=0.0;
+            foreach (Shape shape in shapes)
+                sumArea += shape.GetArea();
+            Console.WriteLine(sumArea);
+        }
+    }
+}
---
+++ Homework3/ConsoleApp5/ConsoleApp5/Shape.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    abstract class Shape
+    {
+        abstract public double GetArea();
+        abstract public bool IsLegal();
+
+    }
+    class Rectangle : Shape
+    {
+        public double Height { set; get; }
+        public double Width { set; get; }
+        public Rectangle(double width, double height)
+        {
+            Width = width;
+            Height = height;
+        }
+        public override double GetArea()
+        {
+            return Width * Height;
+        }
+        public override bool IsLegal()
+        {
+            return Width > 0 && Height > 0;
+        }
+    }
+    class Square : Rectangle
+    {
+        public Square(double width) : base(width, width) { }
+
+        public override bool IsLegal()
+        {
+            return Width == Height && Width > 0 && Height > 0;
+        }
+    }
+    class Triangle : Shape
+    {
+        private double Bottom { set; get; }
+        private double Height { set; get; }
+        public Triangle(double bottom, double height)
+        {
+            Bottom = bottom;
+            Height = height;
+        }
+        public override double GetArea()
+        {
+            return (Bottom * Height) / 2;
+        }
+        public override bool IsLegal()
+        {
+            return Bottom > 0 && Height > 0;
+        }
+
+    }
+}
