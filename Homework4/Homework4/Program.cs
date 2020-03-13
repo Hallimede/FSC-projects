0815d8951eab7a5b81c8064dc8ccb58ad61eae75
Hallimede <1216938422@qq.com>
2020-03-13 10:44:20 +0800

    Homework04

---
+++ Homework4/Homework4/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericApplication
+{
+
+    // 链表节点
+    public class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+
+        public Node(T t)
+        {
+            Next = null;
+            Data = t;
+        }
+    }
+
+    //泛型链表类
+    public class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+
+        public GenericList()
+        {
+            tail = head = null;
+        }
+
+        public Node<T> Head
+        {
+            get => head;
+        }
+
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if (tail == null)
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+        public void ForEach(Action<T> action)
+        {
+            Node<T> node =head;
+           
+            while (node!= null) { 
+                action(node.Data);
+                node = node.Next;
+            }
+            
+            
+        }
+    
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 整型List
+            GenericList<int> intlist = new GenericList<int>();
+            for (int x = 0; x < 10; x++)
+            {
+                intlist.Add(x);
+            }
+            int sum = 0, max = 0, min = 0;
+            intlist.ForEach(x => sum += x);
+            Console.WriteLine($"sum= { sum}");
+            intlist.ForEach(x => { if (max > x) max = x; });
+            Console.WriteLine($"max= { max}");
+            intlist.ForEach(x => { if (min < x) min = x; });
+            Console.WriteLine($"min= { min}");
+
+        }
+
+    }
+}
---
+++ Homework4/Homework4_2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework4_2
+{
+    public delegate void TickHandler(object sendery);
+    public delegate void AlarmHandler(object sender,int x,int y);
+    public class Clock
+    {
+        public event TickHandler StartTick; 
+        public event AlarmHandler StartAlarm;
+        public void Start(int hour,int minute) {
+            while (true) {
+                System.Threading.Thread.Sleep(1000);
+                StartTick(this);
+                if (DateTime.Now.Hour == hour && DateTime.Now.Minute == minute&&DateTime.Now.Second==0)
+                    StartAlarm(this, hour, minute);
+            }
+        }
+    }
+    public class ClockSubscriber
+    {
+        public Clock clock = new Clock();
+        public ClockSubscriber()
+        {
+            clock.StartTick += Tick;
+            clock.StartAlarm += Alarm;
+        }
+        void Tick(object sender)
+        {
+            Console.WriteLine("滴答");
+        }
+        void Alarm(object sender,int hour,int minute)
+        {
+            Console.WriteLine("响铃!!!");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hour, minute;
+            Console.WriteLine("设置闹钟为几时？");
+            hour = int.Parse(Console.ReadLine());
+            Console.WriteLine("设置闹钟为几分？");
+            minute = int.Parse(Console.ReadLine());
+            ClockSubscriber clockSubscriber = new ClockSubscriber();
+            clockSubscriber.clock.Start(hour,minute);
+        }
+    }
+
+    
+}
