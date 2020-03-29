e78a4af8da066be987e2582851c0091498694885
Hallimede <1216938422@qq.com>
2020-03-29 21:59:31 +0800

    dsfsd

---
+++ Homework6/Homework05/Client.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework6
+{
+     public class Client
+    {
+        public string Name{get;set;}
+        public Client() { }
+        public Client(string name) { Name = name; }
+        public override string ToString()
+        {
+            return "      [客户："+Name+"]";
+        }
+    }
+}
