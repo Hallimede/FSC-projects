3ced7a45387d63ce16617d1bfd5c63f0b7307b15
Hallimede <1216938422@qq.com>
2020-05-24 22:03:12 +0800

    homewotk12

---
+++ Homework12/homework12/Controllers/OrderController.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using OrderApp;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace homework12.Controllers
+{
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class OrderController : Controller
+    {
+        private readonly OrderContext orderDatabase;
+        public OrderController(OrderContext context) {
+            this.orderDatabase = context;
+        }
+        
+        // GET请求: api/<controller>
+        [HttpGet]
+        public ActionResult<List<Order>> GetOrder(string merName,string cusName,float? totalAmount) 
+        {
+            IQueryable<Order> query = orderDatabase.Orders;
+
+            if (cusName != null) { 
+                query=query.Where(o => o.Customer.Name == cusName); 
+            }
+            if (totalAmount != null) {
+                query = query.Where(o => o.Items.Sum(item => item.GoodsItem.Price * item.Quantity) > totalAmount);
+            }
+            if (merName != null)
+            {
+                query = query.Where(o => o.Items.Count(i => i.GoodsItem.Name == merName) > 0);
+            }
+            return query.ToList();
+        
+        }
+
+
+        // GET请求 api/<controller>/5
+        [HttpGet("{id}")]
+        public ActionResult<Order> Get(string id)
+        {
+            var order = orderDatabase.Orders.FirstOrDefault(o => o.Id == id);
+            if (order==null)  return NotFound();
+            return order;
+        }
+
+
+        // PUT请求 api/<controller>/5
+        [HttpPut("{id}")]
+        public ActionResult<Order> Put(string id,Order order)
+        {
+            if (id != order.Id) 
+            { 
+                return BadRequest("Id cannot be modified");
+            }
+
+            try 
+            {
+                orderDatabase.Entry(order).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+                orderDatabase.SaveChanges();
+            }
+            catch (Exception e) 
+            {
+                string error = e.Message;
+                if (e.InnerException != null) 
+                    error = e.InnerException.Message;
+                return BadRequest(error);
+            }
+            return NoContent();
+
+        }
+
+        // POST请求 api/<controller>
+        [HttpPost]
+        public ActionResult<Order> Post(Order order)
+        {
+            try
+            {
+                orderDatabase.Orders.Add(order);
+                orderDatabase.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.InnerException.Message);
+            }
+            return order;
+        }
+
+
+        // DELETE请求 api/<controller>/5
+        [HttpDelete("{id}")]
+        public ActionResult Delete(string id)
+        {
+            try {
+                var order = orderDatabase.Orders.FirstOrDefault(t => t.Id == id);
+                if (order != null) {
+                    orderDatabase.Remove(order);
+                    orderDatabase.SaveChanges();
+                }
+            }
+            catch (Exception e) {
+                return BadRequest(e.InnerException.Message);
+            }
+            return NoContent();
+        }
+    }
+}
---
+++ Homework12/homework12/Startup.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using OrderApp;
+using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
+using Pomelo.EntityFrameworkCore.MySql.Internal;
+
+namespace homework12
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection service)
+        {
+            service.AddDbContextPool<OrderContext>(options => options.UseMySql(Configuration.GetConnectionString("OrderDatabase"), MySqlOptions => MySqlOptions.ServerVersion(new Version(8,0,20), ServerType.MySql)));
+            service.AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
